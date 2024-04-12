eb8c39ccccd40ebaccb6aa8cd864096afccde575
faithadewuyi <iyanuleke09@gmail.com>
2024-04-13 00:52:29 +0100

    About page continued

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -18,7 +18,40 @@ const About = () => {
       <button className=" border-2 border-[#06101B] rounded-[6px] px-2 ml-1 gap-1 text-[18px] font-semibold text-white  h-[44px] mt-2 mb-2">Learn More</button>
       <button className="ml-1  border-1 border-[#AF0100] bg-[#AF0100] rounded-[6px] px-2  gap-1 text-[18px] font-semibold text-white h-[44px]  mt-2 mb-2">Get Started</button>
       </div> 
+      <div style={{
+                backgroundImage: `url(home1.png)`,
+                width: '100%', 
+                height: '546px', 
+                opacity: 1 
+            }}
+      className="bg-cover "></div>
       </div>
+      <div className="justify-center text-center gap-[12px]">
+        <p className="font-poppins font-bold text-[18px] text-[#4D4D4D] h-[28px] mt-8">Transform Your Marketplace</p>
+        <p className="text-catamara font-bold text-[36px] mt-2 mb-4">Unlock Your Business Potential</p>
+      </div>
+      <div className="bg-[#D2DCE7] w-[1312px] h-[184px]  flex border-1 border-[#949494] rounded-[12px] flex-row gap-[24px] p-[32px] m-auto">
+        <div className="flex flex-col gap-[12px] p-4">
+          <p className="font-poppins font-bold text-[36px]">15K+</p>
+          <p className="font-poppins font-semibold text-[18px] text-[#949494]">10,000+ Services Offered</p></div>
+        <div className="flex flex-col gap-[12px] p-4">
+          <p className="font-poppins font-bold text-[36px]">20K+</p>
+          <p className="font-poppins font-semibold text-[18px] text-[#949494]">5,000+ Happy Sellers</p>
+          </div>
+        <div className="flex flex-col gap-[12px] p-4">
+          <p className="font-poppins font-bold text-[36px] ">99.9%</p>
+          <p className="font-poppins font-semibold text-[18px] text-[#949494]">95% Customer Satisfaction</p>
+          </div>
+        <div className="flex flex-col gap-[12px] p-4">
+          <p className="font-poppins font-bold text-[36px]">100M</p>
+          <p className="font-poppins font-semibold text-[18px] text-[#949494]">1 Million+ Users Nationwide</p>
+        </div>
+      </div>
+      <div className="justify-center text-center gap-[12px]">
+        <p className="font-poppins font-bold text-[18px] text-[#4D4D4D] h-[28px] mt-8">Explore. Connect. Succeed.</p>
+        <p className="text-catamara font-bold text-[36px] mt-2 mb-4">Revolutionizing Service Marketplaces</p>
+      </div>
+      
      </div> 
     <Footer/>
     </div>
